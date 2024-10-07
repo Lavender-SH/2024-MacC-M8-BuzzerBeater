ad524dd24023047695ae047206e2d27e5f925227
interstellaar <https://github.com/>
2024-10-07 11:24:29 +0900

    gitignore setting

---
+++ BuzzerBeater/BuzzerBeater/BuzzerBeaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzerBeaterApp.swift
+//  BuzzerBeater
+//
+//  Created by Jeho Ahn on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BuzzerBeaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
