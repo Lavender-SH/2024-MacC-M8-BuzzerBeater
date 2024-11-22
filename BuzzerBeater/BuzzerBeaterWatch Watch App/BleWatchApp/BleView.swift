b83c1fe43d0ab055cfd6714c38261013aecff315
interstellaar <https://github.com/>
2024-11-22 18:27:11 +0900

    sensor connect

--- BuzzerBeater/BuzzerBeaterWatch Watch App/BleWatchApp/BleView.swift
+++ BuzzerBeater/BuzzerBeaterWatch Watch App/BleWatchApp/BleView.swift
@@ -10,25 +10,15 @@ import CoreBluetooth
 
 
 struct BleView: View {
+    
     var body: some View {
-
-        NavigationView {
-            List {
-                // NavigationLink로 각각의 뷰를 연결
-                NavigationLink(destination: ConnectView().environmentObject(BleDeviceManager.shared)) {
-                    Text("Connect View")
-                        .font(.headline)
-                        .padding()
-                }
-                NavigationLink(destination: BiasCheckView()) {
-                    Text("Bias Check View")
-                        .font(.headline)
-                        .padding()
-                }
-
-            }
-            .navigationTitle("Sensor Settings") // 네비게이션 바 제목 설정
+        VStack {
+            ConnectView()
+                .environmentObject(BleDeviceManager.shared)
+            
+            BiasCheckView()
         }
+        .padding()
     }
 }
 
